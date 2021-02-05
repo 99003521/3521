f1a75cd85225b74b82e99058c1018d07ad8ca4aa
amanajaykalaskar <amanajay.kalaskar@ltts.com>
2021-02-05 18:06:44 +0530

    update

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -14,24 +14,63 @@ using namespace std;
 int addnum(int a, int b=1){
     return a+b;
 }
-float addnum(float a, float b=1){
+float addnum1(float a, float b=1){
     return a+b;
 }
-
 int addnum1(int &a, int &b){
     return a+b;
 }
+int addnum2(int *a, int *b){
+    return *a+*b;
+}
+
+
 
 int subnum(int a, int b=1){
     return a-b;
 }
-int mulnum(int a, int b){
+float subnum1(float a, float b=1){
+    return a-b;
+}
+int subnum1(int &a, int &b){
+    return a-b;
+}
+int subnum2(int *a, int *b){
+    return *a-*b;
+}
+
+
+
+int mulnum(int a, int b=2){
     return a*b;
 }
+float mulnum1(float a, float b=1){
+    return a*b;
+}
+int mulnum1(int &a, int &b){
+    return a*b;
+}
+int mulnum2(int *a, int *b){
+    return *a*(*b);
+}
+
+
+
 int divnum(double a, double b){
     if(b==0){return -1;}
     return double (a/b);
 }
+float divnum1(float a, float b=1){
+    return a+b;
+}
+int divnum1(int &a, int &b){
+    return a+b;
+}
+int divnum2(int *a, int *b){
+    return *a+*b;
+}
+
+
 /**
  * @brief main1 function
  * 
@@ -42,16 +81,23 @@ int main(){
     // int *x=&a, *y=&b;
     cout << "Addition:\n";
     cout << addnum(a,b) << endl;
-    cout << addnum1(a,b) << endl;
+    cout << addnum1(19.5) << endl;
+    cout << addnum2(&a,&b) << endl;
 
     cout << "Substraction:\n";
     cout << subnum(a,b) << endl;
+    cout << subnum1(19.5) << endl;
+    cout << subnum2(&a,&b) << endl;
 
     cout << "Multilication:\n";
     cout << mulnum(a,b) << endl;
+    cout << mulnum1(19.5) << endl;
+    cout << mulnum2(&a,&b) << endl;
 
     cout << "Division:\n";
     cout << divnum(a,b) << endl;
+    cout << divnum1(19.5) << endl;
+    cout << divnum2(&a,&b) << endl;
     
     return 0;
 }
